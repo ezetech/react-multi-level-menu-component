fc860d0e2371ff9fc04b2f5b31915096227a9948
Eugene Alforov <eugenalforov@gmail.com>
2016-11-18 17:27:40 +0200

    added tests for hovering on items

--- src/menu-list/menu-list.spec.jsx
+++ src/menu-list/menu-list.spec.jsx
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import MenuList from './menu-list.jsx';
 import { flatItems } from './spec-helpers/items-config-mock.js';
+import { deepItems } from './spec-helpers/items-config-mock.js';
 
 describe('Menu List Component', () => {
   it( 'should be empty when items were not passed', () => {
@@ -29,5 +30,27 @@ describe('Menu List Component', () => {
     const itemsNumber = flatItems.length;
     const wrapper = mount(<MenuList show="true" items={flatItems} />);
     expect(wrapper.find('MenuItem').length).to.equal(itemsNumber);
+  });
+});
+describe('Menu List Component - Hover on item', () => {
+    it( 'menu item which have it\'s own items should have inner list when hovered', () => {
+      const itemClass = 'itemClass';
+      const listClass = 'listClass';
+      const itemSelector = '.' + itemClass;
+      const listSelector = '.' + listClass;
+
+      const wrapper = mount(<MenuList show="true" items={deepItems} listClass={listClass} itemClass={itemClass} />);
+      wrapper.find(itemSelector).first().simulate('mouseover');
+      expect(wrapper.find(itemSelector).first().find(listSelector).length).to.equal(1);
+  });
+  it( 'menu item which have not it\'s own items should not have inner list when hovered', () => {
+      const itemClass = 'itemClass';
+      const listClass = 'listClass';
+      const itemSelector = '.' + itemClass;
+      const listSelector = '.' + listClass;
+
+      const wrapper = mount(<MenuList show="true" items={flatItems} listClass={listClass} itemClass={itemClass} />);
+      wrapper.find(itemSelector).first().simulate('mouseover');
+      expect(wrapper.find(itemSelector).first().find(listSelector).length).to.equal(0);
   });
 });
