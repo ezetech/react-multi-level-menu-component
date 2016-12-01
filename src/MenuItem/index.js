662833076a90957004a670090ab4b3f27f118a0d
Ivan Stetsenko <i.stetsenko1@gmail.com>
2016-12-02 01:21:21 +0200

    fix: remove unnecessary renaming during import. fix missed file extension

--- src/MenuItem/index.js
+++ src/MenuItem/index.js
@@ -1,1 +1,1 @@
-export { default as default } from './MenuItem';
+export { default } from './MenuItem';
--- src/MenuList/index.js
+++ src/MenuList/index.js
@@ -1,1 +1,1 @@
-export { default as default } from './MenuList';
+export { default } from './MenuList';
